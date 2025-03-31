3e52f43011ab01cdeb9bcf64c4dd62d18704d177
arthursereno <arthurbarbosa2809@gmail.com>
2025-03-31 10:25:55 -0300

    Update client.js

--- client.js
+++ client.js
@@ -2,10 +2,8 @@ window.TrelloPowerUp.initialize({
   'card-buttons': function(t, options) {
     return [{
       text: '🌐 Comprar Proxy',
-      callback: function(t) {
-        window.open('https://app.privateproxy.me/order/2408?purpose_id=306', '_blank');
-        return t.closePopup(); // fecha o popup do botão
-      }
+      url: 'https://app.privateproxy.me/order/2408?purpose_id=306',
+      target: 'blank' // abre em nova aba
     }];
   }
 });
